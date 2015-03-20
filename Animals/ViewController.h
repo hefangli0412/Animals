db95ffddb2c0d46712f7a7f476d2b84c3dbd234d
hefangli <hefangli0412@gmail.com>
2015-03-20 13:31:37 -0700

    Initial Commit

---
+++ Animals/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animals
+//
+//  Created by Hefang Li on 3/20/15.
+//  Copyright (c) 2015 hefang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Animals/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Animals
+//
+//  Created by Hefang Li on 3/20/15.
+//  Copyright (c) 2015 hefang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
